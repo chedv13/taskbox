718939009f38ea0c81f860cf6560478f924bc0f2
Dmitry Chekurov <chedv13@gmail.com>
2015-09-04 14:08:23 +0400

    added view tests for create, update and delete task.

--- taskbox/taskbox/tests/test_views.py
+++ taskbox/taskbox/tests/test_views.py
@@ -1,7 +1,5 @@
 from django.test import TestCase, RequestFactory
-from taskbox.taskbox.views import *
 from taskbox.taskbox.tests.factories import *
-from django.test import Client
 from django.core.urlresolvers import reverse
 
 import django
@@ -13,12 +11,42 @@ class BaseUserTestCase(TestCase):
     def setUp(self):
         self.user = UserFactory()
         self.factory = RequestFactory()
+        self.client.login(username=self.user.username, password='adm1n')
+
+
+class TestTaskCreateView(BaseUserTestCase):
+    def test_create_task_with_text(self):
+        self.assertEqual(Task.objects.count(), 0)
+        self.client.post(reverse('taskbox:create_task'), {'text': 'test text'})
+        self.assertEqual(Task.objects.count(), 1)
+
+    def test_create_task_without_text(self):
+        self.assertEqual(Task.objects.count(), 0)
+        self.client.post(reverse('taskbox:create_task'), {'text': ''})
+        self.assertEqual(Task.objects.count(), 0)
+
+
+class TestTaskUpdateView(BaseUserTestCase):
+    def test_update_task(self):
+        task = TaskFactory(user=self.user)
+
+        self.assertEqual(Task.objects.count(), 1)
+        self.client.post(reverse('taskbox:update_task', args=(task.id,)), {'text': 'test'})
+        self.assertEqual(Task.objects.first().text, 'test')
+
+
+class TestTaskDeleteView(BaseUserTestCase):
+    def test_delete_task(self):
+        task = TaskFactory(user=self.user)
+
+        self.assertEqual(Task.objects.count(), 1)
+        self.client.delete(reverse('taskbox:delete_task', args=(task.id,)))
+        self.assertEqual(Task.objects.count(), 0)
 
 
 class TestTaskDetailView(BaseUserTestCase):
     def test_detail_view(self):
         task = TaskFactory(user=self.user)
-        self.client.login(username=task.user.username, password='adm1n')
 
         response = self.client.get('/tasks/%d' % (task.id))
         self.assertEqual(response.status_code, 200)
@@ -26,8 +54,6 @@ class TestTaskDetailView(BaseUserTestCase):
 
 class TestTaskListView(BaseUserTestCase):
     def test_index_view_without_tasks(self):
-        self.client.login(username=self.user.username, password='adm1n')
-
         response = self.client.get('/tasks')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Tasks not found")
@@ -35,12 +61,7 @@ class TestTaskListView(BaseUserTestCase):
 
     def test_index_view_wth_tasks(self):
         TaskFactory(user=self.user)
-        self.client.login(username=self.user.username, password='adm1n')
 
         response = self.client.get('/tasks')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['task_list'].count(), 1)
-
-#
-# class TestTaskCreateView():
-#     def test_create_task_with_text(self):
