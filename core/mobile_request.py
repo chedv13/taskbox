8448acc0038103377607b54c9607f2f392d3f258
Dmitry Chekurov <chedv13@gmail.com>
2015-09-09 12:14:52 +0400

    fixed not found key in request.META.

--- core/mobile_request.py
+++ core/mobile_request.py
@@ -3,7 +3,8 @@ from django.shortcuts import render_to_response
 
 class CheckIphoneRequest(object):
     def process_response(self, request, response):
-        if 'iPhone' in request.META['HTTP_USER_AGENT']:
+        if request.META and 'HTTP_USER_AGENT' in request.META \
+                and 'iPhone' in request.META['HTTP_USER_AGENT']:
             return render_to_response('pages/mobile_app_download.html')
 
         return response
