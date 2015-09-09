dd0cb3d46929c9594916273ee5a3a4db9dee9638
Dmitry Chekurov <chedv13@gmail.com>
2015-09-09 17:34:41 +0400

    refactored path and name for mobile checking middleware.

--- taskbox/middleware/mobile_checker.py
+++ taskbox/middleware/mobile_checker.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response
 
 
-class CheckIphoneRequest(object):
+class MobileCheckerMiddleware(object):
     def process_response(self, request, response):
         if request.META and 'HTTP_USER_AGENT' in request.META \
                 and 'iPhone' in request.META['HTTP_USER_AGENT']:
